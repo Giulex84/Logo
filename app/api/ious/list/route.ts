f192a32ea38d76eb51472e96eec38806440c0c79
Giulex84 <giulianom.gm@gmail.com>
2026-01-23 01:10:37 +0100

    Delete app/api directory

--- app/api/ious/list/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    ious: [],
-    note: "This endpoint lists commitment records only",
-  });
-}
--- app/api/pi/mock-payments/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  return NextResponse.json({
-    disabled: true,
-    message: "Payments are not supported by this application",
-  });
-}
